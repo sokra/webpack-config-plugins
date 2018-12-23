3d2054032a68537e55fd4f72a538cd6818f2b515
Jan Nicklas <j.nicklas@me.com>
2018-12-24 00:36:16 +0100

    Webpack 5 Speed Test

--- performance/environments/latest/webpack.config.babel.js
+++ performance/environments/latest/webpack.config.babel.js
@@ -1,6 +1,5 @@
 const ImagesConfigWebpackPlugin = require('image-config-webpack-plugin');
 const JsConfigWebpackPlugin = require('js-config-webpack-plugin');
-const ScssConfigWebpackPlugin = require('scss-config-webpack-plugin');
 
 module.exports = {
 	context: __dirname,
@@ -11,8 +10,5 @@ module.exports = {
 		// Multi threading babel loader configuration with caching for .js and .jsx files
 		// see https://github.com/namics/webpack-config-plugins/tree/master/packages/js-config-webpack-plugin/config
 		new JsConfigWebpackPlugin(),
-		// SCSS Configuration for .css .module.css and .scss .module.scss files
-		// see https://github.com/namics/webpack-config-plugins/tree/master/packages/scss-config-webpack-plugin/config
-		new ScssConfigWebpackPlugin(),
 	],
 };
